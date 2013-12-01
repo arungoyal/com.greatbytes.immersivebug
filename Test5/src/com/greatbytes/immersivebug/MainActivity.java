a9afdc9d272aca062271562a483f5c4b48be1707
Nikolaus Fischer <nikolaus_fischer@t-online.de>
2013-12-01 04:20:51 +0100

    Initial commit

---
+++ Test5/src/com/greatbytes/immersivebug/MainActivity.java
@@ -0,0 +1,35 @@
+package com.greatbytes.immersivebug;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Launch immersive mode
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {  
+		//Close the activity
+		finish();
+		return true;
+        
+	}
+}
